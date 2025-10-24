8a285a775ba3454fb9df01aa49fb564735a0de60
GamingX <GamingX@posteo.de>
2025-10-24 20:37:25 +0200

    piece dragging
    Signed-off-by: GamingX <GamingX@posteo.de>

--- whale/src/whale_app.rs
+++ whale/src/whale_app.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use eframe::{egui, App};
 use crate::chess_parts::{piece_from_u8, Board, Piece};
 
+#[allow(dead_code)]
 fn print_board(board: Board){
     println!("  +---+---+---+---+---+---+---+---+");
     for i in 0..8 {
@@ -29,10 +30,34 @@ fn print_board(board: Board){
     println!("    a   b   c   d   e   f   g   h");
 }
 
+fn piece_name_for_square(board: Board, row: usize, col: usize) -> Option<String> {
+    match board.cells[row * 8 + col] {
+        0 => None,
+        cell => {
+            let (piece, color) = piece_from_u8(cell);
+            let name = match piece {
+                Piece::Pawn => "pawn",
+                Piece::Rook => "rook",
+                Piece::Knight => "knight",
+                Piece::Bishop => "bishop",
+                Piece::Queen => "queen",
+                Piece::King => "king",
+            };
+            Some(if color.into() {
+                format!("white_{}", name)
+            } else {
+                format!("black_{}", name)
+            })
+        }
+    }
+}
+
 pub(crate) struct WhaleApp {
     board: Board,
     image_bytes: Vec<(&'static str, &'static [u8])>,
     textures: HashMap<&'static str, egui::TextureHandle>,
+    dragging_piece: Option<(usize, usize)>,
+    drag_offset: egui::Vec2,
 }
 
 impl WhaleApp {
@@ -54,12 +79,14 @@ impl WhaleApp {
                 ("black_king", include_bytes!("assets/black-king.png")),
             ],
             textures: HashMap::new(),
+            dragging_piece: None,
+            drag_offset: egui::Vec2::ZERO,
         }
     }
 }
 
 impl App for WhaleApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         if self.textures.is_empty() {
             for (name, bytes) in &self.image_bytes {
                 let img = image::load_from_memory(bytes).unwrap().to_rgba8();
@@ -105,34 +132,46 @@ impl App for WhaleApp {
                         );
                         let color = if (row + col) % 2 == 0 { color_a } else { color_b };
                         painter.rect_filled(rect, 0.0, color);
-                        let piece_name = match self.board.cells[row * 8 + col] {
-                            0 => None,
-                            cell => {
-                                let (piece, color) = piece_from_u8(cell);
-                                let name = match piece {
-                                    Piece::Pawn => "pawn",
-                                    Piece::Rook => "rook",
-                                    Piece::Knight => "knight",
-                                    Piece::Bishop => "bishop",
-                                    Piece::Queen => "queen",
-                                    Piece::King => "king",
-                                };
-                                Some(if color.into() {
-                                    format!("white_{}", name)
-                                } else {
-                                    format!("black_{}", name)
-                                })
-                            }
-                        };
-                        if let Some(name) = piece_name {
-                            if let Some(texture) = self.textures.get(name.as_str()) {
-                                painter.image(
-                                    texture.id(),
-                                    rect,
-                                    egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0)),
-                                    egui::Color32::WHITE,
+                        if let Some(piece_name) = piece_name_for_square(self.board.clone(), row, col) {
+                            if let Some(texture) = self.textures.get(piece_name.as_str()) {
+                                let image_rect = egui::Rect::from_min_max(
+                                    egui::pos2(x, y),
+                                    egui::pos2(x + square_size, y + square_size),
                                 );
+                                if Some((row, col)) != self.dragging_piece {
+                                    painter.image(
+                                        texture.id(),
+                                        image_rect,
+                                        egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0)),
+                                        egui::Color32::WHITE,
+                                    );
+                                }
+                                let piece_response = ui.interact(image_rect, ui.id().with((row, col)), egui::Sense::click_and_drag());
+                                if piece_response.drag_started() {
+                                    self.dragging_piece = Some((row, col));
+                                    if let Some(pointer) = ui.ctx().pointer_interact_pos() {
+                                        self.drag_offset = pointer - rect.min;
+                                    }
+                                }
+                                if piece_response.drag_stopped() {
+                                    self.dragging_piece = None;
+                                }
                             }
+                        }
+                    }
+                }
+                if let Some((row, col)) = self.dragging_piece {
+                    let piece_name = piece_name_for_square(self.board.clone(), row, col).unwrap();
+                    if let Some(texture) = self.textures.get(piece_name.as_str()) {
+                        if let Some(pointer) = ui.ctx().pointer_interact_pos() {
+                            let pos = pointer - self.drag_offset;
+                            let drag_rect = egui::Rect::from_min_size(pos, egui::vec2(square_size, square_size));
+                            painter.image(
+                                texture.id(),
+                                drag_rect,
+                                egui::Rect::from_min_max(egui::pos2(0.0,0.0), egui::pos2(1.0,1.0)),
+                                egui::Color32::WHITE,
+                            );
                         }
                     }
                 }
