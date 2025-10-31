e071c12409c894166804459e0bff0685ed092bcf
GamingX <GamingX@posteo.de>
2025-10-31 20:38:49 +0100

    pseudolegal moves for #2
    Signed-off-by: GamingX <GamingX@posteo.de>

--- whale/src/chess_engine.rs
+++ whale/src/chess_engine.rs
@@ -6,13 +6,12 @@ pub(crate) fn generate_pseudolegal(board: &Board, index: Mailbox64Index) -> Vec<
     let moveset = MOVESETS.get(&(piece, color)).unwrap();
     for (dx, dy) in &moveset.0 {
         if moveset.1 {
-            moves.extend(offset_ray_2d(index.clone(), *dx, *dy, 7));
+            moves.extend(offset_ray_2d(board, index.clone(), *dx, *dy, 7));
         } else {
             if let Some(target_index) = offset_index_2d(index.clone(), *dx, *dy) {
                 moves.push(target_index);
             }
         }
     }
-
     moves
 }
